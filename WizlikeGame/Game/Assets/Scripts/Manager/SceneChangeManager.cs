331914349522da6f5cadafaf30b157b511359ff8
kenta-suzuki <kenta.suzuki@befool.co.jp>
2017-04-10 17:40:49 +0900

    シーン遷移周りを作成

---
+++ WizlikeGame/Game/Assets/Scripts/Manager/SceneChangeManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeManager
+{
+	public static void Load(string sceneName, bool UseFade = false)
+	{
+		if (!UseFade)
+		{
+			Load(sceneName);
+			return;
+		}
+
+		FadeInOut.Instance.FadeIn(() => Load(sceneName));
+	}
+
+	static void Load(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public static void LoadAsync(string sceneName)
+	{
+		SceneManager.LoadSceneAsync(sceneName);
+	}
+}
---
+++ WizlikeGame/Game/Assets/Scripts/UI/Common/FadeInOut.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class FadeInOut : MonoBehaviour
+{
+	[SerializeField]
+	Image BG;
+	[SerializeField]
+	byte amount;
+
+	const string PrefabPath = "Prefabs/UI/Fade";
+
+	static FadeInOut _instance;
+	public static FadeInOut Instance { get { return _instance; } }
+
+	void Start()
+	{
+		_instance = this;
+		gameObject.SetActive(false);
+		Application.DontDestroyOnLoad(transform.parent);
+	}
+
+	public void FadeIn(Action callback)
+	{
+		gameObject.SetActive(true);
+		StartCoroutine(WaitForFadeIn(callback));
+	}
+
+	IEnumerator WaitForFadeIn(Action callback)
+	{
+		byte alpha = 0;
+		while (BG.color.a < 1)
+		{
+			alpha += amount;
+			BG.color = new Color32(0, 0, 0, alpha);
+			yield return null;
+		}
+
+		callback();
+		yield return new WaitForSeconds(0.3f);
+		FadeOut();
+	}
+
+	void FadeOut()
+	{
+		StartCoroutine(WaitForFadeOut());
+	}
+
+	IEnumerator WaitForFadeOut()
+	{
+		byte alpha = 255;
+		while (BG.color.a > 0)
+		{
+			alpha -= amount;
+			BG.color = new Color32(0, 0, 0, alpha);
+			yield return null;
+		}
+		gameObject.SetActive(false);
+	}
+}
